b33fff5374c683ff04c66d29c03808289b48e2a0
unknown <eugeneamo85@gmail.com>
2023-02-21 12:46:03 +0000

    chromium path changed to module own

--- config/insta-scrapper.js
+++ config/insta-scrapper.js
@@ -1,21 +1,25 @@
-const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 
+var usernameAndPasswordConfig={
+  username:"genicist__",
+  password:"GenicsInsta"
+}
 async function scrapeInstagram(profile) {
   const browser = await puppeteer.launch({
     headless: false,
-    executablePath: "C:/Users/Genics/Documents/chrome-win/chrome.exe",
+    // product:"firefox",
+    // executablePath: "C:/Users/Genics/Documents/chrome-win/chrome.exe",
+    executablePath: "node_modules/puppeteer/.local-chromium",
   });
   const page = await browser.newPage();
 
-  //   await page.goto("https://www.instagram.com/accounts/login/?source=auth_switcher");
-  //   await page.waitForTimeout(1000);
-  //   await page.type("input[name='username']",usernameAndPasswordConfig.username);
-  //   await page.type("input[name='password']",usernameAndPasswordConfig.password);
-  //   await (await page.$("button[type='submit']")).click();
-  //   await page.waitForTimeout(5000);
+    // await page.goto("https://www.instagram.com/accounts/login/?source=auth_switcher");
+    // new Promise((r) => setTimeout(r, 1000));
+    // await page.type("input[name='username']",usernameAndPasswordConfig.username);
+    // await page.type("input[name='password']",usernameAndPasswordConfig.password);
+    // await (await page.$("button[type='submit']")).click();
+    // new Promise((r) => setTimeout(r, 5000));
   await page.goto(`https://www.instagram.com/${profile}/`);
-  //   await page.waitForTimeout(5000);
   new Promise((r) => setTimeout(r, 5000));
 
   var allImages = await page.evaluate(() => {
@@ -29,8 +33,6 @@ async function scrapeInstagram(profile) {
   });
 
   new Promise((r) => setTimeout(r, 2000));
-
-  // await page.waitForTimeout(2000);
   await page.close();
   return allImages;
 }
