caee8438651edc3718226a868e57521abcb6c283
unknown <eugeneamo85@gmail.com>
2023-02-21 12:56:53 +0000

    ping pong route added

--- routes/images.js
+++ routes/images.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const getInstaImages=require("../controllers/get-image");
 
-router.route("/get-images").post(getInstaImages);
+router.route("/get-images").post(getInstaImages).get((req,res)=>{
+    res.send("pong");
+})
 
 module.exports = router;
